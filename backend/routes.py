786cbc08dc616865c4b8b031040ad665985833d8
I am a cat <joemar.cardino000@gmail.com>
2023-05-24 17:14:11 +0800

    updated base program

--- backend/routes.py
+++ backend/routes.py
@@ -9,3 +9,7 @@ def index():
 @app.route("/main", methods=['GET', 'POST'])
 def main():
     return render_template("main.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
