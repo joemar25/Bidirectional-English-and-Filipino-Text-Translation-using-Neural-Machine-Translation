e287066d9015ca1335f7887c377f27a90674ed90
I am a cat <joemar.cardino000@gmail.com>
2023-05-24 18:16:56 +0800

    working prototype

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, jsonify, request
 from backend.main import app
+from translate import Translator
 
 @app.route("/")
 @app.route("/index")
@@ -13,3 +14,18 @@ def main():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.route('/translate', methods=['POST'])
+def translate_text():
+    # Get the request data
+    request_data = request.get_json()
+    from_language = request_data['fromLanguage']
+    to_language = request_data['toLanguage']
+    text_to_translate = request_data['textToTranslate']
+
+    # Translate the text
+    translator = Translator(from_lang=from_language, to_lang=to_language)
+    translated_text = translator.translate(text_to_translate)
+
+    # Return the translated text
+    return jsonify({'translatedText': translated_text})
